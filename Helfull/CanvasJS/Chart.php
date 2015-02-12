c2d978197a32107ae70c8c11ab3d5a0591cb8f96
Sebastian Kraft <dev@helfull.de>
2015-02-12 19:59:45 +0100

    render chart

--- Helfull/CanvasJS/Chart.php
+++ Helfull/CanvasJS/Chart.php
@@ -2,66 +2,68 @@
 
 namespace Helfull\CanvasJS;
 
-use Illuminate\Support\Collection;
-use Illuminate\Contracts\Support\Renderable;
-use Helfull\CanvasJS\Chart\DataPoint;
 use Helfull\CanvasJS\Chart\ChartData;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Collection;
 use View;
 
-class Chart extends Collection implements Renderable{
+class Chart extends Collection implements Renderable {
 
-    protected $id = 'chart';
-    protected $options = [];
+	protected $id = 'chart';
+	protected $options = [];
 
-    public function __construct($options = []) {
-        parent::__construct($options);
-        $this->resolveID();
-    }
+	public function __construct($opt = []) {
+		$options['chart'] = $opt;
+		parent::__construct($options);
+		$this->resolveID();
+	}
 
-    public function addData(ChartData $chartData){
-        $data = $this->getData();
-        array_push($data, $chartData);
-        $this->setData($data);
-        return $this;
-    }
+	public function addData(ChartData $chartData) {
+		$data = $this->getData();
+		array_push($data, $chartData);
+		$this->setData($data);
+		return $this;
+	}
 
-    public function getData() {
-        return $this->get('data')?:[];
-    }
+	public function getData() {
+		return $this->get('data') ?: [];
+	}
 
-    public function setData($data) {
-        $this->put('data', $data);
-    }
+	public function setData($data) {
+		$this->put('data', $data);
+	}
 
-    public function getChart() {
-        return $this->get('chart');
-    }
+	public function getChart() {
+		return json_encode($this->get('chart'));
+	}
 
-    protected function resolveID() {
-        if(!$this->has('id')) {
-            $this->id = $this->generateID();
-        } else {
-            $this->id = $this->pull('id');
-        }
-    }
+	protected function resolveID() {
+		if (!$this->has('id')) {
+			$this->id = $this->generateID();
+		} else {
+			$this->id = $this->pull('id');
+		}
+	}
 
-    private function generateID() {
-        return uniqid("Chart");
-    }
+	private function generateID() {
+		return uniqid("Chart");
+	}
 
-    public function toArray() {
-        $arr['id'] = $this->id;
-        $arr['chart'] = parent::toArray();
-        return $arr;
-    }
+	public function toArray() {
+		$arr['id'] = $this->id;
+		$arr['chart'] = parent::toArray();
+		return $arr;
+	}
 
-    public function toString() {
-        return $this->toJson();
-    }
+	public function toString() {
+		return $this->toJson();
+	}
 
-    public function render($jquery=false) {
-        $chart = $this;
-        $view = $jquery ? View::make('canvasjs::chartjquery') : View::make('canvasjs::chart');
-        return $view->with(compact('chart'));
-    }
+	public function render($jquery = false) {
+		$chart = $this;
+
+		$view = $jquery ? 'chartjquery' : 'chart';
+
+		return View::make('canvasjs::' . $view, ['chart' => $this]);
+	}
 }
--- Helfull/CanvasJS/Views/chart.blade.php
+++ Helfull/CanvasJS/Views/chart.blade.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">
   window.onload = function () {
-    var {{$chart->id}} = new CanvasJS.Chart("{{$chart->id}}",{{$chart}});
+    var {{$chart->id}} = new CanvasJS.Chart("{{$chart->id}}",{{{$chart->getChart()}}});
 </script>
 
 <div id="{{$chart->id}}" style="height: 300px; width: 100%;"></div>
