257c4a3fa7711ed4ae7c8ab50a83fbdf7c996cd0
Sebastian Kraft <dev@helfull.de>
2015-02-13 21:58:30 +0100

    [add] test for array support

--- tests/ChartTest.php
+++ tests/ChartTest.php
@@ -4,54 +4,57 @@ use Helfull\CanvasJS\Chart;
 
 class ChartTest extends PHPUnit_Framework_TestCase {
 
-    function testGenerateID() {
-        $runs = 10;
-        $resultOld = null;
+	function testGenerateID() {
+		$runs = 10;
+		$resultOld = null;
 
-        for($i = 0; $i<=$runs; $i++){
-            $resultNew = new Chart;
-            if(!is_null($resultOld)) $this->assertNotEquals($resultOld->getID(), $resultNew->getID());
-            $resultOld = null;
-            $resultOld = $resultNew;
-        }
-    }
+		for ($i = 0; $i <= $runs; $i++) {
+			$resultNew = new Chart;
+			if (!is_null($resultOld)) {
+				$this->assertNotEquals($resultOld->getID(), $resultNew->getID());
+			}
 
-    function testInitChartWithID() {
-        $id = 'testChart';
-        $chart = new Chart(['id'=>$id]);
-        $this->assertEquals($id, $chart->getID());
-        $this->assertEquals($id, $chart->id);
-    }
+			$resultOld = null;
+			$resultOld = $resultNew;
+		}
+	}
 
-    function testShouldHaveChartPropertie() {
-        $chartProp = ['animationEnabled'=>true];
-        $chart = new Chart(['chart'=>$chartProp]);
-        $this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->getChart());
-        $this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->chart);
-    }
+	function testInitChartWithID() {
+		$id = 'testChart';
+		$chart = new Chart(['id' => $id]);
+		$this->assertEquals($id, $chart->getID());
+		$this->assertEquals($id, $chart->id);
+	}
 
-    function testShouldAddAttributes() {
-        $attributes = [
-            "style"=>"height:300",
-            "class"=>"chart"
-        ];
-        $chart = new Chart;
-        $chart->putAttribute('style',$attributes['style']);
-        $this->assertEquals($attributes['style'], $chart->getAttribute('style'));
+	function testShouldHaveChartPropertie() {
+		$chartProp = ['animationEnabled' => true];
+		$chart = new Chart(['chart' => $chartProp]);
+		$this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->getChart());
+		$this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->chart);
+	}
 
-        $chart = new Chart;
-        $chart->putAttributes($attributes);
-        $this->assertEquals($attributes['style'], $chart->getAttribute('style'));
-        $this->assertEquals($attributes['class'], $chart->getAttribute('class'));
-    }
+	function testShouldAddAttributes() {
+		$attributes = [
+			"style" => "height:300",
+			"class" => "chart",
+		];
+		$chart = new Chart;
+		$chart->putAttribute('style', $attributes['style']);
+		$this->assertEquals($attributes['style'], $chart->getAttribute('style'));
 
-    function testShouldSupportJSON() {
-        $json = '
-        {"id": "test","chart": 
-            {"data": 
+		$chart = new Chart;
+		$chart->putAttributes($attributes);
+		$this->assertEquals($attributes['style'], $chart->getAttribute('style'));
+		$this->assertEquals($attributes['class'], $chart->getAttribute('class'));
+	}
+
+	function testShouldSupportJSON() {
+		$json = '
+        {"id": "test","chart":
+            {"data":
                 [
                     {
-                        "dataPoints": 
+                        "dataPoints":
                         [
                             {
                                 "label": "banana",
@@ -63,15 +66,23 @@ class ChartTest extends PHPUnit_Framework_TestCase {
                 ]
             }
         }';
-        $jsonArr = json_decode($json, true);
-        $chart = new Chart($json);
-        $this->assertEquals(
-            count($jsonArr['chart']['data']), 
-            $chart->chart->getData()->count()
-        );
+		$jsonArr = json_decode($json, true);
+		$chart = new Chart($json);
+		$this->assertEquals(
+			count($jsonArr['chart']['data']),
+			$chart->chart->getData()->count()
+		);
 
-        $this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->chart);
+		$this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->chart);
 
-    }
+	}
+
+	function testShouldSuportArray() {
+		$arr = ['id' => 'test', 'chart' => ['data' => [['dataPoints' => [["label" => "banana", "y" => 999]]]]]];
+		$chart = new Chart($arr);
+		$this->assertEquals($arr['id'], $chart->getID());
+		$this->assertEquals(count($arr['chart']['data']), $chart->chart->getData()->count());
+		$this->assertInstanceOf('Helfull\CanvasJS\Chart\ChartPropertie', $chart->chart);
+	}
 
 }
