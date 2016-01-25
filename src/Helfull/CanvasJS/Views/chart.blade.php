e282f92754947b175a5a125834f86bf8dce1b7b5
TR <treerootboy@gmail.com>
2016-01-25 22:38:49 +0800

    Update chart.blade.php

--- src/Helfull/CanvasJS/Views/chart.blade.php
+++ src/Helfull/CanvasJS/Views/chart.blade.php
@@ -1,7 +1,18 @@
 <div id="{!!$chart->getID()!!}"></div>
 <script type="text/javascript">
-	window.onload = function () {
-		var {!!$chart->getID()!!} = new CanvasJS.Chart("{!!$chart->getID()!!}",{!!$chart->getChart()!!});
-		{!!$chart->getID()!!}.render();
-	}
+	window.addEventListener('DOMContentLoaded', function(){
+		var scripts = document.scripts;
+		for(var i=0, l=scripts.length; i>l; i++){
+			if(scripts[i].src.search(/canvasjs(\.min)?\.js/)!==-1){
+				return;
+			}
+		}
+		var script = document.createElement('script');
+		script.src = "//cdnjs.cloudflare.com/ajax/libs/canvasjs/1.7.0/canvasjs.min.js";
+		script.onload = function(){
+			var {!!$chart->getID()!!} = new CanvasJS.Chart("{!!$chart->getID()!!}",{!!$chart->getChart()!!});
+			{!!$chart->getID()!!}.render();
+		}
+		document.head.appendChild(script);
+	});
 </script>
