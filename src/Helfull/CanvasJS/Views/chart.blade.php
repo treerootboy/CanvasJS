bde6048788c970ed9b66fb9efd1194c275311f4b
TR <treerootboy@gmail.com>
2016-01-26 02:21:45 +0800

    Update chart.blade.php

--- src/Helfull/CanvasJS/Views/chart.blade.php
+++ src/Helfull/CanvasJS/Views/chart.blade.php
@@ -9,9 +9,32 @@
 		}
 		var script = document.createElement('script');
 		script.src = "//cdnjs.cloudflare.com/ajax/libs/canvasjs/1.7.0/canvasjs.min.js";
+
+		var option = {!!$chart->getChart()!!}
+				, id = "{!!$chart->getID()!!}";
+
 		script.onload = function(){
-			var {!!$chart->getID()!!} = new CanvasJS.Chart("{!!$chart->getID()!!}",{!!$chart->getChart()!!});
+			option.legend = {
+				cursor:"pointer",
+				itemclick : function(e) {
+					if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
+						e.dataSeries.visible = false;
+					} 
+					else {
+						e.dataSeries.visible = true;
+					}
+					{!!$chart->getID()!!}.render();
+					resize();
+				}
+			};
+			var {!!$chart->getID()!!} = new CanvasJS.Chart(id, option);
 			{!!$chart->getID()!!}.render();
+			resize();
+		}
+		var resize = function(){
+			var container = document.querySelector('#'+id);
+			var canvas = document.querySelector('#'+id+' canvas');
+			container.style.height = canvas.height+'px';
 		}
 		document.head.appendChild(script);
 	});
