7e98627d8c99bd6a0d304be36d67d911b11c29e9
TR <treerootboy@gmail.com>
2016-01-26 12:45:24 +0800

    Update chart.blade.php

--- src/Helfull/CanvasJS/Views/chart.blade.php
+++ src/Helfull/CanvasJS/Views/chart.blade.php
@@ -11,8 +11,16 @@
 		var script = document.createElement('script');
 		script.src = "//cdnjs.cloudflare.com/ajax/libs/canvasjs/1.7.0/canvasjs.min.js";
 
-		var option = {!!$chart->getChart()!!}
-				, id = "{!!$chart->getID()!!}";
+		var option = '{!!$chart->getChart()!!}'
+			, id = '{!!$chart->getID()!!}'
+			, date_type = '{!!Request::input("date_type", "%Y-%m-%d")!!}'
+			, dateMap = {
+				'%Y-%m-%d': [ /"x"\s*:\s*"(\d{4}-\d{1,2}-\d{1,2})"/g, '"x":new Date("$1")' ],
+				'%Y-%m': [ /"x"\s*:\s*"(\d{4}-\d{1,2})"/g, '"label":"$1"' ],
+				'%Y': [ /"x"\s*:\s*"(\d{4})"/g, '"label":"$1"' ]
+			};
+			
+		option = eval('('+option.replace.apply(option, dateMap[date_type])+')');
 
 		script.onload = function(){
 			option.legend = {
